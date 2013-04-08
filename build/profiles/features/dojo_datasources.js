8524ffa530722d32ce73505ef61cd1a15d60fea1
Patrick Ruzand <pruzand@gmail.com>
2013-04-08 18:02:17 +0200

    add dojo-dnd and dojo-datasources layers

---
+++ build/profiles/features/dojo_datasources.js
@@ -0,0 +1,15 @@
+dojo_datasources = [
+	"dojo/data/ItemFileReadStore",
+	"dojo/data/ItemFileWriteStore",
+	"dojo/data/ObjectStore",
+	"dojo/data/util/filter",
+	"dojo/data/util/simpleFetch",
+	"dojo/data/util/sorter",
+	"dojo/store/Cache",
+	"dojo/store/DataStore",
+	"dojo/store/JsonRest",
+	"dojo/store/Memory",
+	"dojo/store/Observable",
+	"dojo/store/util/QueryResults",
+	"dojo/store/util/SimpleQueryEngine"
+];
---
+++ build/profiles/features/dojo_dnd.js
@@ -0,0 +1,15 @@
+dojo_dnd = [
+	"dojo/dnd/autoscroll",
+	"dojo/dnd/AutoSource",
+	"dojo/dnd/Avatar",
+	"dojo/dnd/common",
+	"dojo/dnd/Container",
+	"dojo/dnd/Manager",
+	"dojo/dnd/move",
+	"dojo/dnd/Moveable",
+	"dojo/dnd/Mover",
+	"dojo/dnd/Selector",
+	"dojo/dnd/Source",
+	"dojo/dnd/Target",
+	"dojo/dnd/TimedMoveable"
+];
