2d2089f07b2bdf1a163b5b5edb930ed20a8b51c8
Patrick Ruzand <pruzand@gmail.com>
2013-09-02 16:24:08 +0200

    add 1.9 binaries

---
+++ releases/env-config.js
@@ -0,0 +1,51 @@
+var profile = {
+	"action"				  : "release",
+
+	// point basePath to build/
+	"basePath"				  : "../",
+	"releaseDir"			  : "./tmp",
+	"selectorEngine"		  : "acme",
+	"optimize"				  : "closure",
+	"layerOptimize"			  : "closure",
+	"stripConsole"			  : "normal",
+	"copyTests"				  : false,
+	
+	"cssImportIgnore"		  : "../dijit.css",
+	"cssOptimize"			  : "comments.keepLines",
+	"mini"					  : true,
+	"webkitMobile"			  : true,
+	// specificies a list of locale to generate flattened nls bundle, or set
+	// it to null to rely on new 1.7 behavior
+	"localeList"			  : "en-us",
+	// specifies the flattened bundles to copy in the layers (it avoids a separate file and separate request)
+	//"includeLocales"		  : ["en-us"],
+	
+	// comment out these options if you load the layers with <script> tag (and you should not!)
+	// instead of require(..) them.
+	//"insertAbsMids"			  : true,
+	//"compat" : 1.6,
+	//"noref" : true,
+	
+	packages:[
+		{ name:"dojo", location:"./src/dojo"},
+		{ name:"dojox", location:"./src/dojox"},
+		{ name:"dijit", location:"./src/dijit"},
+		{
+			name:"gridx",
+			location:"./src/gridx",
+			resourceTags: {
+				ignore: function(filename, mid){
+					return /gridx\/gallery\//.test(mid) || /gridx\/mobile\/util\//.test(mid);
+				},
+				test: function(filename, mid){
+					return /\/tests\//.test(mid);
+				},
+				amd: function(filename, mid){
+					return !/\/tests\//.test(mid) &&
+						/\.js$/.test(filename) &&
+						!/\w+_\w+/.test(filename);
+				}
+			}
+		}
+	]
+}
